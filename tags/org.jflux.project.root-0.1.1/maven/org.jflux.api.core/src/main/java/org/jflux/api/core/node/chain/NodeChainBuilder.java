ce8bc6e7568fb6984d165577f0bcee85815bd1a1
matt.r.stevenson <matt.r.stevenson@5bd3f42d-16c3-419c-a79d-a6b79fb88cdc>
2012-05-11 18:57:20 +0000

    [maven-release-plugin]  copy for tag org.jflux.project.root-0.1.1

---
+++ tags/org.jflux.project.root-0.1.1/maven/org.jflux.api.core/src/main/java/org/jflux/api/core/node/chain/NodeChainBuilder.java
@@ -0,0 +1,127 @@
+/*
+ *  Copyright 2012 by The JFlux Project (www.jflux.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jflux.api.core.node.chain;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jflux.api.core.node.*;
+import org.jflux.api.core.util.Adapter;
+import org.jflux.api.core.util.Listener;
+import org.jflux.api.core.util.Notifier;
+
+/**
+ *
+ * @author Matthew Stevenson <www.jflux.org>
+ */
+public class NodeChainBuilder<H,T> {
+    private ProducerNode myProducer;
+    private List<ProcessorNode> myProcessorList;
+    private Class<T> myTailClass;
+    
+    public static <C> NodeChainBuilder<C,C> build(Class<C> head){
+        return new NodeChainBuilder<C, C>(head,head);
+    }
+    
+    public static <C> NodeChainBuilder<C,C> build(
+            Class<C> head, Notifier<C> producer){
+        return build(new DefaultProducerNode<C>(head, producer));
+    }
+    
+    public static <C> NodeChainBuilder<C,C> build(ProducerNode<C> producer){
+        if(producer == null){
+            throw new NullPointerException();
+        }
+        Class<C> c = producer.getProducedClass();
+        NodeChainBuilder<C, C> builder = new NodeChainBuilder<C, C>(c,c);
+        builder.myProducer = producer;
+        return builder;
+    }
+    
+    private NodeChainBuilder(Class<H> head, Class<T> tail){
+        if(head == null || tail == null){
+            throw new NullPointerException();
+        }
+        myProcessorList = new ArrayList<ProcessorNode>();
+        myTailClass = tail;
+    }
+    
+    public <N> NodeChainBuilder<H,N> attach(
+            Class<N> outputClass, Adapter<T,N> adapter){
+        if(outputClass == null || adapter == null){
+            throw new NullPointerException();
+        }
+        return attach(new DefaultProcessorNode<T, N>(
+                myTailClass, outputClass, adapter));
+    }
+    
+    public <N> NodeChainBuilder<H,N> attach(ProcessorNode<T,N> proc){
+        if(proc == null){
+            throw new NullPointerException();
+        }else if(!proc.getConsumedClass().isAssignableFrom(myTailClass)){
+            throw new IllegalArgumentException(
+                    "Expected class: " + myTailClass 
+                    + ", found class: " + proc.getConsumedClass());
+        }
+        myProcessorList.add(proc);
+        NodeChainBuilder<H,N> newChainBuilder = (NodeChainBuilder<H,N>)this;
+        newChainBuilder.myTailClass = proc.getProducedClass();
+        return newChainBuilder;
+    }
+    
+    public NodeChain attach(Listener<T> consumer){
+        if(consumer == null){
+            throw new NullPointerException();
+        }
+        return attach(new DefaultConsumerNode<T>(myTailClass, consumer));
+    }
+    
+    public NodeChain attach(ConsumerNode<T> consumer){
+        if(consumer == null){
+            throw new NullPointerException();
+        }else if(!consumer.getConsumedClass().isAssignableFrom(myTailClass)){
+            throw new IllegalArgumentException(
+                    "Expected class: " + myTailClass 
+                    + ", found class: " + consumer.getConsumedClass());
+        }
+        if(myProducer == null){
+            return getConsumerChain(consumer);
+        }else if(consumer == null){
+            return getProducerChain();
+        }
+        return new NodeChain(myProducer, myProcessorList, consumer);
+    }
+    
+    public NodeChain getNodeChain(){
+        return new NodeChain(myProducer, myProcessorList);
+    }
+    
+    public ConsumerChain<H> getConsumerChain(Listener<T> listener){
+        return getConsumerChain(
+                new DefaultConsumerNode<T>(myTailClass, listener));
+    }
+    
+    public ConsumerChain<H> getConsumerChain(ConsumerNode<T> node){
+        return new ConsumerChain(myProcessorList, node);
+    }
+    
+    public ProcessorChain<H,T> getProcessorChain(){
+        return new ProcessorChain(myProcessorList);
+    }
+    
+    public ProducerChain<T> getProducerChain(){
+        return new ProducerChain(myProducer, myProcessorList);
+    }
+}
---
+++ tags/org.jflux.project.root-0.1.1/maven/org.jflux.api.data/src/main/java/org/jflux/api/data/schedule/HeartbeatNode.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2012 by The JFlux Project (www.jflux.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jflux.api.data.schedule;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jflux.api.core.node.DefaultProducerNode;
+import org.jflux.api.core.playable.Playable.PlayState;
+import org.jflux.api.core.util.DefaultNotifier;
+import org.jflux.api.core.util.Factory;
+import org.jflux.api.core.util.Notifier;
+
+/**
+ *
+ * @author Matthew Stevenson <www.jflux.org>
+ */
+public class HeartbeatNode<M> extends 
+        DefaultProducerNode<M> implements ScheduleNode<M,TimeUnit> {
+    private final static Logger theLogger = Logger.getLogger(HeartbeatNode.class.getName());
+    
+    private Factory<M> myFactory;
+    private ScheduledExecutorService myExecutor;
+    private long myInitialDelay;
+    private long myPeriod;
+    private TimeUnit myTimeUnit;
+    private ScheduledFuture<?> myFuture;
+    
+    public HeartbeatNode(Class<M> msgClass, Factory<M> factory,
+            long initialDelay, long period, TimeUnit timeUnit){
+        super(msgClass, new DefaultNotifier<M>());
+        if(factory == null || timeUnit == null){
+            throw new NullPointerException();
+        }
+        myInitialDelay = initialDelay;
+        myPeriod = period;
+        myTimeUnit = timeUnit;
+        myFactory = factory;
+    }
+    
+    public synchronized void setPeriod(long period){
+        myPeriod = period;
+        if(PlayState.RUNNING != getPlayState()){
+            return;
+        }
+        long delay = myInitialDelay;
+        myInitialDelay = 0L;
+        pause();
+        start();
+        myInitialDelay = delay;
+    }
+
+    @Override
+    public boolean start() {
+        if(myFuture != null){
+            if(myFuture.isDone()){
+                myFuture = null;
+            }else{
+                return true;
+            }
+        }
+        if(myExecutor == null){
+            myExecutor = new ScheduledThreadPoolExecutor(1);   
+        }
+        myFuture = myExecutor.scheduleAtFixedRate(
+                new MessagePump(), myInitialDelay, myPeriod, myTimeUnit);
+        return super.start();
+    }
+
+    @Override
+    public boolean pause() {
+        if(myFuture != null){
+            myFuture.cancel(false);
+            myFuture = null;
+        }
+        return super.pause();
+    }
+
+    @Override
+    public boolean resume() {
+        return start();
+    }
+
+    @Override
+    public boolean stop() {
+        if(myFuture != null){
+            myFuture.cancel(false);
+            myFuture = null;
+            myExecutor.shutdown();
+            myExecutor = null;
+        }
+        return super.stop();
+    }
+    
+    class MessagePump implements Runnable {
+
+        @Override
+        public void run() {
+            if(myFactory == null || getPlayState() != PlayState.RUNNING){
+                return;
+            }
+            M t = myFactory.build();
+            Notifier<M> n = getNotifier();
+            if(n == null || t == null){
+                return;
+            }
+            try{
+                n.notifyListeners(t);
+            }catch(RuntimeException ex){
+                theLogger.log(Level.WARNING, 
+                        "Runtime Exception in Heartbeat Node", ex);
+            }
+        }
+        
+    }
+}
