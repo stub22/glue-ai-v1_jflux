68faa93700020fe5e6a5d4830fecc8ea01a27884
matt.r.stevenson <matt.r.stevenson@5bd3f42d-16c3-419c-a79d-a6b79fb88cdc>
2012-05-11 17:39:45 +0000

    [maven-release-plugin]  copy for tag org.jflux.project.root-0.1.1

---
+++ tags/org.jflux.project.root-0.1.1/maven/org.jflux.api.core/src/main/java/org/jflux/api/core/node/chain/NodeChain.java
@@ -0,0 +1,189 @@
+/*
+ *  Copyright 2012 by The JFlux Project (www.jflux.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jflux.api.core.node.chain;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jflux.api.core.node.ConsumerNode;
+import org.jflux.api.core.node.Node;
+import org.jflux.api.core.node.ProcessorNode;
+import org.jflux.api.core.node.ProducerNode;
+import org.jflux.api.core.playable.Playable;
+import org.jflux.api.core.playable.PlayableGroup;
+import org.jflux.api.core.util.Notifier;
+
+/**
+ *
+ * @author Matthew Stevenson <www.jflux.org>
+ */
+public class NodeChain<P,C> extends PlayableGroup implements Node{
+    private ProducerNode<P> myProducer;
+    private List<ProcessorNode<?,?>> myProcessors;
+    private ConsumerNode<C> myConsumer;
+    private List<Playable> myPlayables;
+    private boolean myWiredFlag;
+    
+    NodeChain(List<ProcessorNode<?,?>> chain){
+        if(chain == null){
+            throw new NullPointerException();
+        }
+        build(null, chain, null);
+    }
+    
+    NodeChain(ProducerNode<P> producer, List<ProcessorNode<?,?>> chain){
+        if(producer == null || chain == null){
+            throw new NullPointerException();
+        }
+        build(producer, chain, null);
+    }
+    
+    NodeChain(List<ProcessorNode<?,?>> chain, ConsumerNode<C> consumer){
+        if(chain == null || consumer == null){
+            throw new NullPointerException();
+        }
+        build(null, chain, consumer);
+    }
+    
+    NodeChain(ProducerNode<P> producer, 
+            List<ProcessorNode<?,?>> chain, ConsumerNode<C> consumer){
+        if(producer == null || consumer == null){
+            throw new NullPointerException();
+        }
+        build(producer, chain, consumer);
+    }
+    
+    private void build(
+            ProducerNode<P> producer, 
+            List<ProcessorNode<?,?>> chain,
+            ConsumerNode<C> consumer){
+        myProducer = producer;
+        myProcessors = chain;
+        myConsumer = consumer;
+        myWiredFlag = false;
+        collectPlayables();
+        ensureNodeCompatibility();
+    }
+    
+    private void collectPlayables(){
+        int len = myProducer == null ? 0 : 1;
+        len = myConsumer == null ? len : len + 1;
+        len = myProcessors == null ? len : len + myProcessors.size();
+        
+        myPlayables = new ArrayList<Playable>(len);
+        if(myProducer != null){
+            myPlayables.add(myProducer);
+        }
+        if(myProcessors != null){
+            myPlayables.addAll(myProcessors);
+        }
+        if(myConsumer != null){
+            myPlayables.add(myConsumer);
+        }
+    }
+    
+    private void ensureNodeCompatibility(){
+        if(myProducer == null 
+                && (myProcessors == null || myProcessors.isEmpty())){
+            return;
+        }
+        Class produced = myProducer != null ? myProducer.getProducedClass()
+                : myProcessors.get(0).getConsumedClass();
+        if(myProcessors != null){
+            for(ProcessorNode<?,?> proc : myProcessors){
+                verfiyTypes(produced, proc.getConsumedClass());
+                produced = proc.getProducedClass();
+            }
+        }
+        if(myConsumer != null){
+            verfiyTypes(produced, myConsumer.getConsumedClass());
+        }
+    }
+    
+    private static void verfiyTypes(Class<?> input, Class<?> consumed){
+        if(!consumed.isAssignableFrom(input)){
+            throw new IllegalArgumentException(
+                    input + " is not assignable from " + consumed);
+        }
+    }
+    
+    protected ProducerNode<P> getProducer(){
+        return myProducer;
+    }
+    
+    protected List<ProcessorNode<?,?>> getProcessorChain(){
+        return myProcessors;
+    }
+    
+    protected ConsumerNode<C> getConsumer(){
+        return myConsumer;
+    }
+    
+    @Override
+    public boolean start(){
+        if(!myWiredFlag){
+            wire();
+        }
+       return super.start();
+    }
+    
+    protected void wire(){
+        Notifier n = null;
+        if(myProducer != null){
+            n = myProducer.getNotifier();
+            if(myProducer instanceof NodeChain){
+                ((NodeChain)myProducer).wire();
+            }
+        }
+        for(ProcessorNode<?,?> proc : myProcessors){
+            if(proc == null){
+                continue;
+            }
+            n = n == null ? proc.getNotifier() : wire(n, proc);
+            if(proc instanceof NodeChain){
+                ((NodeChain)proc).wire();
+            }
+        }
+        if(myConsumer != null){
+            wire(n,myConsumer);
+            if(myConsumer instanceof NodeChain){
+                ((NodeChain)myConsumer).wire();
+            }
+        }
+        myWiredFlag = true;
+    }
+    
+    private <N,T> Notifier<N> wire(Notifier<T> n, ProcessorNode<T,N> proc){
+        if(proc == null){
+            return null;
+        }
+        if(n != null){
+            n.addListener(proc.getListener());
+        }
+        return proc.getNotifier();
+    }
+    
+    private <T> void wire(Notifier<T> n, ConsumerNode<T> consumer){
+        if(n == null || consumer == null){
+            return;
+        }
+        n.addListener(consumer.getListener());
+    }
+
+    @Override
+    protected Iterable<Playable> getPlayables() {
+        return myPlayables;
+    }
+}
---
+++ tags/org.jflux.project.root-0.1.1/maven/org.jflux.impl.messaging/src/main/java/org/jflux/impl/messaging/JMSAvroUtils.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012 by The JFlux Project (www.jflux.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jflux.impl.messaging;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import javax.jms.BytesMessage;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
+import org.jflux.api.core.node.chain.ConsumerChain;
+import org.jflux.api.core.node.chain.NodeChainBuilder;
+import org.jflux.api.core.node.chain.ProducerChain;
+import org.jflux.api.core.util.Adapter;
+import org.jflux.impl.messaging.avro.AvroDecoder;
+import org.jflux.impl.messaging.avro.AvroEncoder;
+import org.jflux.impl.messaging.avro.AvroEncoder.ByteOutputStreamFactory;
+import org.jflux.impl.messaging.avro.PortableEvent;
+import org.jflux.impl.messaging.avro.PortableEvent.PortableAdapter;
+import org.jflux.impl.messaging.jms.JMSMessageReceiver;
+import org.jflux.impl.messaging.jms.JMSMessageSender;
+import org.jflux.impl.messaging.jms.MessagePacker;
+import org.jflux.impl.messaging.jms.MessageUnpacker;
+
+/**
+ *
+ * @author Matthew Stevenson <www.jflux.org>
+ */
+public class JMSAvroUtils {
+    
+    public static <T, R extends IndexedRecord> 
+            ConsumerChain<T> buildEventSenderChain(
+            Class<T> eventClass, Class<R> recordClass, Schema schema, 
+            Adapter<T,R> eventAdapter, Session session, Destination dest, 
+            Adapter<BytesMessage,BytesMessage> optionalProc)
+            throws JMSException{
+        return NodeChainBuilder.build(eventClass)
+                .attach(recordClass, eventAdapter)
+                .attach(ByteArrayOutputStream.class, 
+                        AvroEncoder.buildBinaryEncoder(recordClass, 
+                                schema, new ByteOutputStreamFactory()))
+                .getConsumerChain(
+                        buildJMSSenderChain(session, dest, optionalProc));
+    }
+    
+    public static <T extends PortableEvent<R>, R extends IndexedRecord> 
+            ConsumerChain<T> buildPortableEventSenderChain(
+            Class<T> eventClass, Class<R> recordClass, Schema schema, 
+            Session session, Destination dest, 
+            Adapter<BytesMessage,BytesMessage> optionalProc)
+            throws Exception{
+        return buildEventSenderChain(eventClass, recordClass, schema, 
+                new PortableAdapter<T, R>(), session, dest, optionalProc);
+    }
+    
+    public static <T extends IndexedRecord> 
+            ConsumerChain<T> buildJMSAvroSenderChain(
+            Class<T> recordClass, Schema schema, 
+            Session session, Destination dest, 
+            Adapter<BytesMessage,BytesMessage> optionalProc)
+            throws Exception{
+        return NodeChainBuilder.build(recordClass)
+                .attach(ByteArrayOutputStream.class, 
+                        AvroEncoder.buildBinaryEncoder(recordClass, 
+                                schema, new ByteOutputStreamFactory()))
+                .getConsumerChain(
+                        buildJMSSenderChain(session, dest, optionalProc));
+    }
+    
+    public static ConsumerChain<ByteArrayOutputStream> buildJMSSenderChain(
+            Session session, Destination dest, 
+            Adapter<BytesMessage,BytesMessage> optionalProc)
+            throws JMSException {
+        NodeChainBuilder<ByteArrayOutputStream,BytesMessage> builder = 
+                NodeChainBuilder.build(ByteArrayOutputStream.class)
+                    .attach(BytesMessage.class, new MessagePacker(session));
+        if(optionalProc != null){
+            builder.attach(BytesMessage.class, optionalProc);
+        }
+        return builder.getConsumerChain(new JMSMessageSender(session, dest));
+    }
+    
+    public static <E, R extends IndexedRecord> 
+            ProducerChain<E> buildEventReceiverChain(
+            Class<E> eventClass, Class<R> recordClass, Schema schema, 
+            Adapter<R,E> recordAdapter, Session session, Destination dest) 
+            throws JMSException {
+        return NodeChainBuilder.build(new JMSMessageReceiver(session, dest))
+                .attach(ByteArrayInputStream.class, new MessageUnpacker())
+                .attach(recordClass, AvroDecoder.buildBinaryDecoder(
+                        ByteArrayInputStream.class, recordClass, schema))
+                .attach(eventClass, recordAdapter)
+                .getProducerChain();
+    }
+    
+    public static <T extends IndexedRecord> 
+            ProducerChain<T> buildJMSAvroReceiverChain(
+            Class<T> recordClass, Schema schema, 
+            Session session, Destination dest) 
+            throws JMSException {
+        return NodeChainBuilder.build(new JMSMessageReceiver(session, dest))
+                .attach(ByteArrayInputStream.class, new MessageUnpacker())
+                .attach(recordClass, AvroDecoder.buildBinaryDecoder(
+                        ByteArrayInputStream.class, recordClass, schema))
+                .getProducerChain();
+    }
+}
