30c7f20f9329adec49b3105be151309514fb5a99
mjacquote <mjacquote@5bd3f42d-16c3-419c-a79d-a6b79fb88cdc>
2015-06-27 16:48:30 +0000

    Changed property key for one of the ServiceFrame lifecycle bindings. Removed appdapter xload depenedency.  Got rid of old code comments

--- maven/org.jflux.demo.services/src/main/java/org/jflux/demo/services/Activator.java
+++ maven/org.jflux.demo.services/src/main/java/org/jflux/demo/services/Activator.java
@@ -1,7 +1,6 @@
 package org.jflux.demo.services;
 
 import org.osgi.framework.BundleContext;
-import org.appdapter.xload.rspec.URLRepoSpec;  // Using this class requires us to import  appdapter.xload
 import org.appdapter.osgi.core.BundleActivatorBase;
 import org.jflux.spec.services.Test2Go;
 import org.jflux.impl.services.rk.lifecycle.ManagedService;
@@ -10,10 +9,7 @@ import org.jflux.impl.services.rk.lifecycle.utils.SimpleLifecycle;
 import java.util.Properties;
 
 public class Activator extends BundleActivatorBase {
-    //private final static String MERGED_MODEL_MANAGER_QN = "csi:merged_manager_1001";
-    
-	
-	
+
 	//SERVICE_MODEL_PATH is the turtle file with the information for the services to be launched.
 	//SVN_MGMT_ONTOLOGY_MODEL_PATH is the ontology that SERVICE_MODEL_PATH uses. 
 	private final static String SERVICE_MODEL_PATH = "file:../org.jflux.spec.services/src/main/resources/org/jflux/spec/services/ServiceTest.ttl";
@@ -32,7 +28,7 @@ public class Activator extends BundleActivatorBase {
        Test2Go test = new Test2Go();
 	   
 	   Properties props = new Properties();
-	   props.put("bundleContextSpec", "http://www.w3.org/2002/07/owl#bundleContextSpec");
+	   props.put("bundleContext", "http://www.w3.org/2002/07/owl#bundleContextSpec");
 	   
 	   ManagedService ms = new OSGiComponent(context, new SimpleLifecycle(context, BundleContext.class), props);
 	   ms.start();
@@ -46,20 +42,3 @@ public class Activator extends BundleActivatorBase {
     public void stop(BundleContext context) throws Exception {
     }
 }
-
-
-// List<ClassLoader> classloaders =
-//                ClassLoaderUtils.getFileResourceClassLoaders(
-//                context, ClassLoaderUtils.ALL_RESOURCE_CLASSLOADER_TYPES);
-//        RepoSpec repoSpec =
-//             new URLRepoSpec("HeadlessMessaging/dir.ttl", classloaders);
-//        
-//        String QUERY_SOURCE_GRAPH_QN = "ccrt:qry_sheet_77";
-//        String TGT_GRAPH_SPARQL_VAR = "qGraph";
-//        Repo.WithDirectory bmcMemoryRepoHandle = repoSpec.makeRepo();
-//        EnhancedRepoClient enhancedRepoSpec = 
-//                new EnhancedLocalRepoClient(repoSpec, bmcMemoryRepoHandle,
-//                        TGT_GRAPH_SPARQL_VAR, QUERY_SOURCE_GRAPH_QN);
-//
-//        RegisterWiring.loadAndRegisterSpec(context, enhancedRepoSpec, MERGED_MODEL_MANAGER_QN);
-//        RegisterWiring.startSpecExtender(context, null);
