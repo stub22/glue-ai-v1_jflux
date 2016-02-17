69fdb8eedc86acf00b5b8ed8b3628c06b305eda2
Ben Jenkins <benjenkinsv95@gmail.com>
2016-02-17 21:55:37 +0000

    Changing packet received from println to trace method.

--- maven/org.jflux.spec.discovery/src/main/java/org/jflux/spec/discovery/Discoverer.java
+++ maven/org.jflux.spec.discovery/src/main/java/org/jflux/spec/discovery/Discoverer.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jflux.spec.discovery;
 
 import java.io.IOException;
@@ -30,92 +29,93 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Runs SSDP discovery to find services.
+ *
  * @author Amy Jessica Book <jgpallack@gmail.com>
  */
-
 public class Discoverer extends DefaultNotifier<UniqueService>
-    implements Runnable {
+        implements Runnable {
+
     private final static String theSsdpAddr = "239.255.255.250";
     private final static int theSsdpPort = 1900;
-    private final static Logger theLogger =
-            LoggerFactory.getLogger(Discoverer.class);
+    private final static Logger theLogger
+            = LoggerFactory.getLogger(Discoverer.class);
 
     @Override
     public void run() {
         InetAddress group;
         MulticastSocket sock;
-        
+
         try {
             group = InetAddress.getByName(theSsdpAddr);
-        } catch(UnknownHostException ex) {
+        } catch (UnknownHostException ex) {
             theLogger.error(
                     "Unknown host " + theSsdpAddr + ": " + ex.getMessage());
             return;
         }
-        
+
         try {
             sock = new MulticastSocket(theSsdpPort);
             sock.joinGroup(group);
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             theLogger.error("Error creating socket: " + ex.getMessage());
             return;
         }
-        
-        while(true) {
+
+        while (true) {
             boolean ourService = false;
             String serial = null;
             Map<String, String> properties = new HashMap<String, String>();
             byte[] buf = new byte[1000];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             String data;
-            
+
             try {
                 sock.receive(packet);
-            } catch(IOException ex) {
+            } catch (IOException ex) {
                 theLogger.error("Can't receive data: " + ex.getMessage());
                 continue;
             }
 
             String ipAddress = packet.getAddress().getHostAddress();
-            
+
             try {
                 data = new String(packet.getData(), "UTF-8");
-            } catch(UnsupportedEncodingException ex) {
+            } catch (UnsupportedEncodingException ex) {
                 theLogger.error("Unsupported encoding: " + ex.getMessage());
                 continue;
             }
-			System.out.println("Packet Received: " + data);
-            
+            theLogger.trace("Packet Received: {}", data);
+
             String[] lines = data.split("\n");
-            
-            for(String line: lines) {
-                if(line.startsWith("USN")) {
-                    if(line.substring(5).trim().equals("JFlux")) {
+
+            for (String line : lines) {
+                if (line.startsWith("USN")) {
+                    if (line.substring(5).trim().equals("JFlux")) {
                         ourService = true;
                     } else {
                         break;
                     }
-                } else if(line.startsWith("LOCATION")) {
+                } else if (line.startsWith("LOCATION")) {
                     String serialNumber = line.split(" ")[1];
                     String[] splitSerial = serialNumber.split(";");
-                    serial =
-                            splitSerial[0].replaceAll("%20", " ")
-                                    .replaceAll("%25", "%");
-                    
-                    for(int i = 1; i < splitSerial.length; i++) {
+                    serial
+                            = splitSerial[0].replaceAll("%20", " ")
+                            .replaceAll("%25", "%");
+
+                    for (int i = 1; i < splitSerial.length; i++) {
                         String[] property = splitSerial[i].split(",");
-                        String key =
-                                property[0].replaceAll("%20", " ")
-                                        .replaceAll("%25", "%");
-                        String value =
-                                property[1].replaceAll("%20", " ")
-                                        .replaceAll("%25", "%");
+                        String key
+                                = property[0].replaceAll("%20", " ")
+                                .replaceAll("%25", "%");
+                        String value
+                                = property[1].replaceAll("%20", " ")
+                                .replaceAll("%25", "%");
                         properties.put(key, value);
                     }
                 }
             }
-            
-            if(ourService && serial != null) {
+
+            if (ourService && serial != null) {
                 notifyListeners(
                         new UniqueService(ipAddress, serial, properties));
             }
